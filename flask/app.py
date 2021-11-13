472d50a8d1f27753c2579fb0ae60640b126974d3
manojkmgit <39760825+manojkmgit@users.noreply.github.com>
2021-11-13 21:34:40 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,8 @@ from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='172.21.0.2', port=6379)
-#cache = redis.Redis(host='127.0.0.1', port=6380)
+#or use just the name of the redis container
+cache = redis.Redis(host='redis_host', port=6379)
 
 def get_hit_count():
     retries = 5
